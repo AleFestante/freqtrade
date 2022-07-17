f62180244dd9860d6b46613481fa89b6e9d0c17a
afestante <afestante@izettle.com>
2022-07-17 17:18:17 +0200

    update

---
+++ scripts/ai_example.sh
@@ -0,0 +1,3 @@
+freqtrade backtesting \
+--config config_examples/config_freqai_spot.example.json \
+--strategy FreqaiExampleStrategy --freqaimodel CatboostPredictionModel --strategy-path freqtrade/templates --timerange 20220101-20220201
